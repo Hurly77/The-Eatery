a52863e14a39d32ac34c4f48a52f1b8df71b934c
cameron <camrbo@gmail.com>
2021-07-09 18:00:34 -0700

    added route for /menu

---
+++ pages/menu/index.js
@@ -0,0 +1,9 @@
+const MenuPage = () => {
+	return (
+		<div className="w-full bg-black flex justify-center pt-3 pb-3">
+			<img src="/images/menu.png" className="lg:w-1/2" />
+		</div>
+	);
+};
+
+export default MenuPage;

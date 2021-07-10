c039db5c53dd487e0a7445b0bf3f80c68b5ed713
cameron <camrbo@gmail.com>
2021-07-09 17:59:52 -0700

    added route for /company

---
+++ pages/company/index.js
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+const CompanyPage = () => {
+  return (
+    <div className='flex justify-center bg-black pt-3'>
+      <img src='/images/time-line.png' className='' />
+    </div>
+  );
+};
+
+export default CompanyPage;

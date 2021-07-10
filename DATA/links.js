c0334146eb49568f3858e8ffcf16a196219ac834
cameron <camrbo@gmail.com>
2021-07-09 17:59:09 -0700

    added simple static data folder

---
+++ DATA/links.js
@@ -0,0 +1,8 @@
+const links = [
+  { id: 1, location: '/', name: 'Home' },
+  { id: 2, location: '/menu', name: 'Menu' },
+  { id: 3, location: '/locations', name: 'Locations' },
+  { id: 4, location: '/company', name: 'Company' },
+];
+
+export default links;
---
+++ DATA/media.js
@@ -0,0 +1,7 @@
+export const media = [
+	['twitter', 'https://twitter.com'],
+	['facebook', 'https://facebook.com'],
+	['instagram', 'https://instagram.com'],
+];
+
+export default media;
